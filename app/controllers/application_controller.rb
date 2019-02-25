63000b9cacb1025fab86fa43f27caf586b9cab16
mitsugu <8964inp.step@gmail.com>
2019-02-25 22:10:47 +0900

    Initialize repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html:"hello,world!"
+    render html: "hello, world!"
   end
+
+  def index
+  @users = User.all
+end
+
 end
